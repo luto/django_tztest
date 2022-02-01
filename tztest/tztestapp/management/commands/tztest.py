c96168128a5f7ab2774c86e025e23448b3abca7f
luto <m@luto.at>
2022-02-01 16:00:05 +0100

    show object representations instead of __str__ output

--- tztest/tztestapp/management/commands/tztest.py
+++ tztest/tztestapp/management/commands/tztest.py
@@ -8,9 +8,8 @@ class Command(BaseCommand):
         posted = timezone.now()
         p1 = Post.objects.create(posted=posted)
         p2 = Post.objects.get(pk=p1.pk)
-        print(p1.posted)
-        print(p1.posted.tzinfo)
-        print(p2.posted)
-        print(p2.posted.tzinfo)
+        print(repr(p1.posted))
+        print(repr(p1.posted.tzinfo))
+        print(repr(p2.posted))
+        print(repr(p2.posted.tzinfo))
         print("equal", p1.posted == p2.posted)
-
