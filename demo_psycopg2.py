8309b4329dec75093f65fc08b1eff8af69e638fe
luto <m@luto.at>
2022-02-02 01:28:57 +0100

    align psycopg2 with django SQL statements

--- demo_psycopg2.py
+++ demo_psycopg2.py
@@ -3,9 +3,7 @@
 import psycopg2
 import datetime
 
-import zoneinfo
-
-posted = datetime.datetime(2022, 2, 2, 18, 30, tzinfo=zoneinfo.ZoneInfo("Europe/Berlin"))
+posted = datetime.datetime(2022, 2, 2, 0, 25, 59, 256973, tzinfo=datetime.timezone.utc)
 
 conn = psycopg2.connect(
     host="127.0.0.1",
@@ -18,8 +16,18 @@ conn = psycopg2.connect(
 with conn.cursor() as cursor:
     cursor.execute("CREATE TABLE IF NOT EXISTS psycopg2_post (posted timestamp with time zone)")
     cursor.execute("TRUNCATE TABLE psycopg2_post;")
-    cursor.execute("INSERT INTO psycopg2_post (posted) VALUES(%s::timestamptz)", (posted,))
-    cursor.execute("SELECT posted::timestamptz FROM psycopg2_post")
+    # django does:
+    #   INSERT INTO "tztestapp_post" ("posted")
+    #   VALUES ('2022-02-02T00:25:59.256973+00:00'::timestamptz)
+    #   RETURNING "tztestapp_post"."id";
+    #   args=(datetime.datetime(2022, 2, 2, 0, 25, 59, 256973, tzinfo=datetime.timezone.utc),)
+    cursor.execute("INSERT INTO psycopg2_post (posted) VALUES(%s::timestamptz);", (posted,))
+    # django does:
+    #   SELECT "tztestapp_post"."id", "tztestapp_post"."posted"
+    #   FROM "tztestapp_post"
+    #   WHERE "tztestapp_post"."id" = 3 LIMIT 21;
+    #   args=(3,);
+    cursor.execute("SELECT posted FROM psycopg2_post;")
     posted_db = cursor.fetchone()[0]
 
 print(posted)
--- tztest/tztestapp/management/commands/tztest.py
+++ tztest/tztestapp/management/commands/tztest.py
@@ -2,9 +2,14 @@ from django.core.management.base import BaseCommand
 from ...models import Post
 from datetime import datetime
 from django.utils import timezone
+import logging
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
+        l = logging.getLogger('django.db.backends')
+        l.setLevel(logging.DEBUG)
+        l.addHandler(logging.StreamHandler())
+
         posted = timezone.now()
         p1 = Post.objects.create(posted=posted)
         p2 = Post.objects.get(pk=p1.pk)
